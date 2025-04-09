81edf37e65f033120bedc9ea43dd0dde8ee136c3
supawat46 <yesnook9999@gmail.com>
2025-04-09 14:41:20 +0700

    .

--- KnnwithHeart.py
+++ KnnwithHeart.py
@@ -6,22 +6,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title('การจำแนกข้อมูลด้วยเทคนิค ML')
-st.image("./img/moew.jpg")
+#st.image("./img/moew.jpg")
 
-col1, col2, = st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
-   st.header("Versicolor")
-   st.image("./img/iris1.jpg")
+   st.header("อิอิ")
+   st.image("./img/h1.jpg")
 
 with col2:
-   st.header("Verginiga")
-   st.image("./img/iris2.jpg")
-
+   st.header("การทำนายโรคหัวใจ")
+   st.image("./img/h2.jpg")
 
 html_7 = """
 <div style="background-color:#c5f18a;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h5></center>
+<center><h5>การทำนายโรคหัวใจ</h5></center>
 </div>
 """
 st.markdown(html_7, unsafe_allow_html=True)
@@ -29,7 +28,7 @@ st.markdown("")
 st.markdown("")
 
 st.subheader("ข้อมูลส่วนแรก 10 แถว")
-dt = pd.read_csv("./data/iris-3.csv")
+dt = pd.read_csv("./data/Heart3.csv")
 st.write(dt.head(10))
 st.subheader("ข้อมูลส่วนสุดท้าย 10 แถว")
 st.write(dt.tail(10))
@@ -45,13 +44,13 @@ feature = st.selectbox("เลือกฟีเจอร์", dt.columns[:-1])
 # วาดกราฟ boxplot
 st.write(f"### 🎯 Boxplot: {feature} แยกตามชนิดของดอกไม้")
 fig, ax = plt.subplots()
-sns.boxplot(data=dt, x='variety', y=feature, ax=ax)
+sns.boxplot(data=dt, x='HeartDisease', y=feature, ax=ax)
 st.pyplot(fig)
 
 # วาด pairplot
 if st.checkbox("แสดง Pairplot (ใช้เวลาประมวลผลเล็กน้อย)"):
     st.write("### 🌺 Pairplot: การกระจายของข้อมูลทั้งหมด")
-    fig2 = sns.pairplot(dt, hue='variety')
+    fig2 = sns.pairplot(dt, hue='HeartDisease')
     st.pyplot(fig2)
 
 html_8 = """
@@ -62,8 +61,17 @@ html_8 = """
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
-pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
-pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
+a1 = st.number_input("กรุณาเลือกข้อมูล a1")
+a2 = st.number_input("กรุณาเลือกข้อมูล a2")
+a3 = st.number_input("กรุณาเลือกข้อมูล a3")
+a4 = st.number_input("กรุณาเลือกข้อมูล a4")
+a5 = st.number_input("กรุณาเลือกข้อมูล a5")
+a6 = st.number_input("กรุณาเลือกข้อมูล a6")
+a7 = st.number_input("กรุณาเลือกข้อมูล a7")
+a8 = st.number_input("กรุณาเลือกข้อมูล a8")
+a9 = st.number_input("กรุณาเลือกข้อมูล a9")
+a10 = st.number_input("กรุณาเลือกข้อมูล a10")
+a11 = st.number_input("กรุณาเลือกข้อมูล a11")
 
 sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
@@ -71,22 +79,20 @@ sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.
 if st.button("ทำนายผล"):
     #st.write("ทำนาย")
    dt = pd.read_csv("./data/iris-3.csv") 
-   X = dt.drop('variety', axis=1)
-   y = dt.variety   
+   X = dt.drop('HeartDisease', axis=1)
+   y = dt.HeartDisease   
 
    Knn_model = KNeighborsClassifier(n_neighbors=3)
    Knn_model.fit(X, y)  
     
-   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   x_input = np.array([[a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11]])
    st.write(Knn_model.predict(x_input))
    
    out=Knn_model.predict(x_input)
 
-   if out[0] == 'Setosa':
-    st.image("./img/iris1.jpg")
-   elif out[0] == 'Versicolor':       
-    st.image("./img/iris2.jpg")
+   if out[0] == 1:
+    st.image("./img/h1.jpg")
    else:
-    st.image("./img/iris3.jpg")
+    st.image("./img/h3.jpg")
 else:
     st.write("ไม่ทำนาย")
